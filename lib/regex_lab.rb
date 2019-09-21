37cd9a60dd5032872ead445c97b63307bc79e0c8
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-21 19:48:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   newArray = text.split()
   finalArray = []
   newArray.each(){ |word| 
-    if word.start_with?("un") && word.end_with?("ing")
+    if word.start_with?("un"bination with the .match and .scan methods. Run learn and build out your methods in regex_lab.rb) && word.end_with?("ing")
       finalArray.push(word)
     end 
   }
