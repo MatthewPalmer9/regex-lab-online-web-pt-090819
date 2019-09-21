8752e9dc1f2c21ae9215b9b2439de8a3ac28eda6
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-21 20:13:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -35,7 +35,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.length == 10 && phone.include?((0..9))
+  if phone.length == 10 && phone.include?(0..9)
     p true
   elsif phone.include?((a..z) || (/[!@#$%^&*()_-+=]/))
     p false 
